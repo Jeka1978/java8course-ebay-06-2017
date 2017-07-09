1d53d2db2c45f7326c1d4350f160a3e0d834b028
bsevgeny@gmail.com <papakita2009>
2017-07-09 15:10:17 +0300

    lab 2 - ended

--- src/main/java/lab2/EmployeeUtil.java
+++ src/main/java/lab2/EmployeeUtil.java
@@ -1,6 +1,7 @@
 package lab2;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by Evegeny on 09/07/2017.
@@ -8,6 +9,10 @@ import java.util.List;
 public class EmployeeUtil {
 
     public static int totalSalary(List<Employee> employees) {
-        return 0;
+        /*Optional<Integer> optional = employees.stream().map(Employee::getSalary).reduce((a, b) -> a + b);
+        return optional.orElse(0);*/
+
+       return employees.stream().mapToInt(Employee::getSalary).sum();
+
     }
 }
