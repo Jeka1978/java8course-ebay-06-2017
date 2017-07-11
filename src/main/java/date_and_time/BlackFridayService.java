fee390086628c1640d41006cd4b8f94824ec296e
bsevgeny@gmail.com <papakita2009>
2017-07-11 14:18:47 +0300

    black friday lab

--- src/main/java/date_and_time/BlackFridayService.java
+++ src/main/java/date_and_time/BlackFridayService.java
@@ -1,10 +1,14 @@
 package date_and_time;
 
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 /**
  * Created by Evegeny on 11/07/2017.
  */
 public class BlackFridayService {
     public void printBlackFridaysQuantity(int startYear, int endYear) {
-
+        Stream<Integer> stream = Stream.iterate(startYear, i -> i + 1).limit(endYear-startYear);
+//        IntStream.range(startYear,endYear)
     }
 }
